25a1c26ea850687fbb57f69bebfcfeea62fcc7af
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2020-02-04 02:11:27 +0900

    board에 쿼리 정렬하기

--- Backend/KMU_likelion/Board/views.py
+++ Backend/KMU_likelion/Board/views.py
@@ -7,25 +7,25 @@ from .pagination import *
 
 #스터디 게시판 viewset
 class StudyViewSet(viewsets.ModelViewSet):
-    queryset = StudyBoard.objects.all()
+    queryset = StudyBoard.objects.all().order_by('pub_date')
     serializer_class = StudySerializer
     pagination_class = Studypagination
 
 #공지 게시판 viewset
 class NoticeViewSet(viewsets.ModelViewSet):
-    queryset = NoticeBoard.objects.all()
+    queryset = NoticeBoard.objects.all().order_by('pub_date')
     serializer_class = NoticeSerializer
     pagination_class = Noticepagination
 
 # QnA 게시판 viewset
 class QnAViewSet(viewsets.ModelViewSet):
-    queryset = QnABoard.objects.all()
+    queryset = QnABoard.objects.all().order_by('pub_date')
     serializer_class = QnASerializer
     pagination_class = QnApagination
 
 # 팀원모집 게시판 viewset
 class RecuitViewSet(viewsets.ModelViewSet):
-    queryset = RecuitBoard.objects.all()
+    queryset = RecuitBoard.objects.all().order_by('pub_date')
     serializer_class = RecuitSerializer 
     pagination_class = Recuitpagination
 
@@ -33,24 +33,24 @@ class RecuitViewSet(viewsets.ModelViewSet):
 
 #스터디 댓글 viewset
 class StudyCommentViewSet(viewsets.ModelViewSet):
-    queryset = StudyComments.objects.all()
+    queryset = StudyComments.objects.all().order_by('pub_date')
     serializer_class = StudyCommentSerializer
     pagination_class = StudyCommentpagination
 
 #공지 댓글 viewset
 class NoticeCommentViewSet(viewsets.ModelViewSet):
-    queryset = NoticeComments.objects.all()
+    queryset = NoticeComments.objects.all().order_by('pub_date')
     serializer_class = NoticeCommentSerializer
     pagination_class = NoticeCommentpagination
 
 # QnA 댓글 viewset
 class QnACommentViewSet(viewsets.ModelViewSet):
-    queryset = QnAComments.objects.all()
+    queryset = QnAComments.objects.all().order_by('pub_date')
     serializer_class = QnACommentSerializer
     pagination_class = QnACommentpagination
 
 # 팀원모집 댓글 viewset
 class RecuitCommentViewSet(viewsets.ModelViewSet):
-    queryset = RecuitComments.objects.all()
+    queryset = RecuitComments.objects.all().order_by('pub_date')
     serializer_class = RecuitCommentSerializer
     pagination_class = RecuitCommentpagination
