c515b48de826060a64515e59e5ffa23997d69c7f
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2020-02-08 17:32:05 +0900

    backend project remodeling

---
+++ Backend/KMU_likelion/accounts/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    major = models.CharField(max_length = 200, null=True)
+    student_id = models.CharField(max_length = 200, null=True)
+    user_type=models.IntegerField(null=True)
+    start_number=models.CharField(max_length = 200, null=True)
+    sns_id=models.CharField(max_length = 200, null=True)
+   
+class Mentoring(models.Model):
+    pub_date = models.DateField(auto_now_add=True)
+    mentor = models.ForeignKey(User, on_delete = models.CASCADE, related_name= "mentor")
+    mentee = models.ForeignKey(User, on_delete = models.CASCADE, related_name= "mentee")
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length= 200)
+    contents = models.TextField()
+    link = models.TextField()
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE, related_name="user_portfolio",default = None)
+
+
+class StudyGroup(models.Model):
+    name = models.CharField(max_length=100) 
+    pub_date = models.DateField(auto_now_add=True)
+    introduction = models.TextField()
+
+class Group_User(models.Model):
+    user_id = models.ForeignKey(User, on_delete= models.CASCADE,null=True)
+    group_id = models.ForeignKey(StudyGroup, on_delete= models.CASCADE,null=True)
+    is_captain = models.BooleanField(default = False)
+    
--- Backend/KMU_likelion/main/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import *
-from rest_framework import serializers
-
-class HashTagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HashTag
-        fields = '__all__'
