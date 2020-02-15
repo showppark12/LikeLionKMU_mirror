656d5d74eb4326ac0d90351875ac514f44c933f6
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2020-02-15 20:39:11 +0900

    calender 삭제 overriding, notice is_valid_date false로 바꾸기

--- Backend/KMU_likelion/Main/views.py
+++ Backend/KMU_likelion/Main/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions,status
 from .models import *
+from Board.models import *
 from .serializers import *
 from rest_framework.decorators import action,api_view
 from rest_framework.response import Response
 from .filterings import *
+from django.http import Http404
 
 # Create your views here.
 class CareerViewSet(viewsets.ModelViewSet):
@@ -18,6 +20,20 @@ class CalendarViewSet(viewsets.ModelViewSet):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
     filter_class=CalendarFilter
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            notice = NoticeBoard.objects.get(id = instance.notice_id.id)
+            print("notice 객체:",notice)
+            print("notice의 boolean", notice.is_valid_date)
+            notice.is_valid_date = False
+            notice.save()
+            print("notice의 boolean", notice.is_valid_date)
+            self.perform_destroy(instance)
+        except Http404:
+            pass
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
