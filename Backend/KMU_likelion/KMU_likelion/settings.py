06f03bf7306e5383ac9914c827e69188562d2203
Aqudi <gjdigj@naver.com>
2020-02-20 01:14:39 +0900

    Change default pagination page size to 20

--- Backend/KMU_likelion/KMU_likelion/settings.py
+++ Backend/KMU_likelion/KMU_likelion/settings.py
@@ -157,5 +157,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 20
 }
