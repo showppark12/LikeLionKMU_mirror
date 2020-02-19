43c52c63309a88b5cb2905a779fb1a85895af3b3
jongmin park <showppark12@naver.com>
2020-02-19 21:46:37 +0900

    mypage이런개

--- Frontend/frontend-react/src/components/accounts/mypage/MyLike.jsx
+++ Frontend/frontend-react/src/components/accounts/mypage/MyLike.jsx
@@ -2,17 +2,35 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
 import api from "../../../api/BoardAPI";
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import Typography from '@material-ui/core/Typography';
-// import PropTypes from 'prop-types';
-// import { makeStyles } from '@material-ui/core/styles';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import {FixedSizeList} from 'react-window';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
-export default class MyLike extends Component {
+
+const useStyles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 300,
+
+  },
+  listSection: {
+    backgroundColor: 'inherit',
+  },
+  ul: {
+    backgroundColor: 'inherit',
+    padding: 0,
+  },
+});
+
+
+
+class MyLike extends React.Component {
   state = {
     likeNotice: [],
     likeQnA: [],
@@ -52,64 +70,91 @@ export default class MyLike extends Component {
       .catch(err => console.log(err));
   }
 
+  show(type){
+    console.log("dsfsdf", type)
+    let LikeList = [];
+    let board_type = "";
+    switch(type){
+      case "Notice Board":
+        LikeList = this.state.likeNotice;
+        board_type = "notice";
+        break;
+      case "QnA Board":
+        LikeList = this.state.likeQnA;
+        board_type = "qna";
+        break;
+      case "Study Board":
+        LikeList = this.state.likeStudy;
+        board_type = "study";
+        break;
+      case "Recruit Board":
+        LikeList = this.state.likeRecruit;
+        board_type = "recruit";
+        break;
+      default:
+        LikeList=[];
+        board_type = "";
+        break;
+    }
+
+    if(board_type === "study"){
+      return(
+        LikeList.map(item => (
+          <ListItem>
+            <Link
+                  to={`/${board_type}/${item.group_name}/detail/${item.id}`}
+                  className={"main-postTitle"}
+                >
+                  <ListItemText primary={`${item.title}`} />
+                </Link>
+            
+          </ListItem>
+        ))
+      )
+    }
+    
+    return(
+      LikeList.map(item => (
+        <ListItem>
+          <Link
+                to={`/${board_type}/detail/${item.id}`}
+                className={"main-postTitle"}
+              >
+                <ListItemText primary={`${item.title}`} />
+              </Link>
+          
+        </ListItem>
+      ))
+    )
+    
+  }
+
+  
+
   render() {
-    // const { id, title, body, purpose } = this.props;
+    const { classes } = this.props;
+
+
 
     return (
       <Paper elevation={10} className="MyLike">
         <>
           <h1>Liked Post</h1>
-          <br />
-          {this.state.likeNotice.map(liked_post => (
-            <div>
-              <h4>Notice board</h4>
-
-              <Link
-                to={`/notice/detail/${liked_post.id}`}
-                className={"main-postTitle"}
-              >
-                -{liked_post.title}
-              </Link>
-            </div>
-          ))}
-          <br />
-          {this.state.likeQnA.map(liked_post => (
-            <div>
-              <h4>QnA board</h4>
-              <Link
-                to={`/qna/detail/${liked_post.id}`}
-                className={"main-postTitle"}
-              >
-                -{liked_post.title}
-              </Link>
-            </div>
-          ))}
-          <br />
-          {this.state.likeStudy.map(liked_post => (
-            <div>
-              <h4>study board</h4>
-              <Link
-                to={`/study/detail/${liked_post.id}`}
-                className={"main-postTitle"}
-              >
-                -{liked_post.title}
-              </Link>
-            </div>
-          ))}
-          <br />
-          {this.state.likeRecruit.map(liked_post => (
-            <div>
-              <h4>recruit board</h4>
-              <Link
-                to={`/recruit/detail/${liked_post.id}`}
-                className={"main-postTitle"}
-              >
-                -{liked_post.title}
-              </Link>
-            </div>
-          ))}
-        </>
+          <br/>
+          <List className={classes.root} subheader={<li />}>
+            {["Notice Board", "QnA Board", "Study Board","Recruit Board"].map(sectionId => (
+              <li key={`section-${sectionId}`} className={classes.listSection}>
+                <ul className={classes.ul}>
+                  <ListSubheader><h3>{`${sectionId}`}</h3></ListSubheader>
+                  {this.show(sectionId)}
+                </ul>
+              </li>
+            ))}
+          </List>
+          </>
       </Paper>
     );
   }
 }
+
+export default withStyles(useStyles)(MyLike)
