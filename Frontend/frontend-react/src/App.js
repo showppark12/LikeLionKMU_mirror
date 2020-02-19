2bdca7d582c6639e86d9f3a5741b0c6fb3b3ad7d
labiss96 <cxz9080@gmail.com>
2020-02-19 20:31:33 +0900

    header navbar 변경. (material-ui => bootstrap)

--- Frontend/frontend-react/src/App.js
+++ Frontend/frontend-react/src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/main/Main";
 
-import Login from "./components/accounts/login";
+import Login from "./components/accounts/Login";
 import Mypage from "./components/accounts/mypage/Mypage";
 
 import Store from "./store/store";
