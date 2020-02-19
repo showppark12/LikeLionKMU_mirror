2d15369e5cb4e3625f379979b550ae8801ec3fd5
Aqudi <gjdigj@naver.com>
2020-02-19 22:29:41 +0900

    Applying convension on accounts

--- Backend/KMU_likelion/accounts/serializers.py
+++ Backend/KMU_likelion/accounts/serializers.py
@@ -4,7 +4,8 @@ from django.contrib.auth import authenticate
 
 from Board.serializers import *
 
-#회원가입
+
+# 회원가입
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -12,18 +13,18 @@ class CreateUserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            validated_data["email"], None, validated_data["password"]
-        )
+        user = User.objects.create_user(validated_data["email"], None, validated_data["password"])
         return user
+
 
 # 접속 유지중인지 확인
 class UserSerializer(serializers.ModelSerializer):
-     class Meta:
-         model = User
-         fields = '__all__'
+    class Meta:
+        model = User
+        fields = '__all__'
 
-# 유저의 활동 내역(글, 댓글)을 포함  
+
+# 유저의 활동 내역(글, 댓글)을 포함
 class UserActivitySerializer(serializers.ModelSerializer):
     studyboard = StudySerializer(many=True, source="studyboard_set")
     noticeboard = NoticeSerializer(many=True, source="noticeboard_set")
@@ -37,11 +38,11 @@ class UserActivitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["studyboard", "noticeboard", "qnaboard", "recruitboard", "studycomments", 
-        "noticecomments", "qnacomments", "recruitcomments"]
+        fields = ["studyboard", "noticeboard", "qnaboard", "recruitboard",
+                  "studycomments", "noticecomments", "qnacomments", "recruitcomments"]
 
 
-#로그인
+# 로그인
 class LoginUserSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
@@ -56,38 +57,41 @@ class LoginUserSerializer(serializers.Serializer):
 class StudyGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudyGroup
-        fields  = '__all__'
+        fields = '__all__'
 
 
 class PortfolioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Portfolio
-        fields  = '__all__'
-
+        fields = '__all__'
 
 
 class Group_UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group_User
-        fields  = '__all__'
+        fields = '__all__'
+
 
 class MentoringSerializer(serializers.ModelSerializer):
     mentor_name = serializers.ReadOnlyField(source='mentor.username')
-    mentee_name = serializers.ReadOnlyField(source = 'mentee.username')
+    mentee_name = serializers.ReadOnlyField(source='mentee.username')
+
     class Meta:
         model = Mentoring
-        fields = ['id','pub_date','mentor','mentee','mentor_name','mentee_name']
+        fields = ['id', 'pub_date', 'mentor', 'mentee', 'mentor_name', 'mentee_name']
 
 
 class MentorSerializer(serializers.ModelSerializer):
     user = UserSerializer(source='mentor')
+
     class Meta:
         model = Mentoring
-        fields  = ['user']
+        fields = ['user']
 
 
 class MenteeSerializer(serializers.ModelSerializer):
     user = UserSerializer(source='mentee')
+
     class Meta:
         model = Mentoring
-        fields  = ['user']
+        fields = ['user']
--- Backend/KMU_likelion/accounts/views.py
+++ Backend/KMU_likelion/accounts/views.py
@@ -11,13 +11,6 @@ from .models import *
 from .filterings import *
 
 
-
-# Create your views here.
-# @api_view(["GET"])
-# def HelloAPI(request):
-#     return Response("hello world!")
-
-
 class RegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
 
@@ -28,14 +21,7 @@ class RegistrationAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response(
-            {
-                "user": UserSerializer(
-                    user, context=self.get_serializer_context()
-                ).data,
-                "token": AuthToken.objects.create(user)[1],
-            }
-        )
+        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data, "token": AuthToken.objects.create(user)[1], })
 
 
 class LoginAPI(generics.GenericAPIView):
@@ -45,22 +31,15 @@ class LoginAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
-        return Response(
-            {
-                "user": UserSerializer(
-                    user, context=self.get_serializer_context()
-                ).data,
-                "token": AuthToken.objects.create(user)[1],
-            }
-        )
+        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data, "token": AuthToken.objects.create(user)[1], })
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    action_serializer_classes = {
-        "activity": UserActivitySerializer
-    }
-    filter_class=UserFilter
+    action_serializer_classes = {"activity": UserActivitySerializer}
+    filter_class = UserFilter
+
     def get_serializer_class(self):
         return self.action_serializer_classes.get(self.action, self.serializer_class)
 
@@ -70,33 +49,35 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(user)
         return Response(serializer.data)
 
+
 class StudyGroupViewSet(viewsets.ModelViewSet):
     queryset = StudyGroup.objects.all().order_by('pub_date')
     serializer_class = StudyGroupSerializer
-    filter_class=StudyGroupFilter
+    filter_class = StudyGroupFilter
 
 
 class PortfolioViewSet(viewsets.ModelViewSet):
-    queryset =Portfolio.objects.all()
+    queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
-    filter_class=PortfolioFilter
+    filter_class = PortfolioFilter
 
 
-class Group_UserViewSet(viewsets.ModelViewSet):
-    queryset =Group_User.objects.all()
-    serializer_class = Group_UserSerializer
-    filter_class=Group_UserFilter
+class GroupUserViewSet(viewsets.ModelViewSet):
+    queryset = GroupUser.objects.all()
+    serializer_class = GroupUserSerializer
+    filter_class = GroupUserFilter
+
 
 class MentoringViewSet(viewsets.ModelViewSet):
     queryset = Mentoring.objects.all()
     serializer_class = MentoringSerializer
-    filter_class=MentoringFilter
+    filter_class = MentoringFilter
 
     @action(detail=False, methods=["GET"])
     def get_mentors(self, request, *args, **kwargs):
-        queryset=Mentoring.objects.exclude(mentor=None)
-        querylist =[]
-        #중복제거
+        queryset = Mentoring.objects.exclude(mentor=None)
+        querylist = []
+        # 중복제거
         for qs in queryset:
             flags = False
             if not querylist:
@@ -108,15 +89,15 @@ class MentoringViewSet(viewsets.ModelViewSet):
                         flags = True
             if flags == False:
                 querylist.append(qs)
-                
-        serializer=MentorSerializer(querylist,many=True)
+
+        serializer = MentorSerializer(querylist, many=True)
         return Response(serializer.data)
 
     @action(detail=False, methods=["GET"])
     def get_mentees(self, request, *args, **kwargs):
-        queryset=Mentoring.objects.exclude(mentee=None)
-        querylist =[]
-        #중복제거
+        queryset = Mentoring.objects.exclude(mentee=None)
+        querylist = []
+        # 중복제거
         for qs in queryset:
             flags = False
             if not querylist:
@@ -128,20 +109,18 @@ class MentoringViewSet(viewsets.ModelViewSet):
                         flags = True
             if flags == False:
                 querylist.append(qs)
-                
-        serializer=MenteeSerializer(querylist,many=True)
+
+        serializer = MenteeSerializer(querylist, many=True)
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         check = serializer.validated_data
-        mentoring_log = Mentoring.objects.filter(mentor = check['mentor'].id)
+        mentoring_log = Mentoring.objects.filter(mentor=check['mentor'].id)
         for mentor in mentoring_log:
             if mentor.mentee.id == check['mentee'].id:
-                return Response(status=status.HTTP_400_BAD_REQUEST) 
+                return Response(status=status.HTTP_400_BAD_REQUEST)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-
