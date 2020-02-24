01553279d211c276e17907f75813552678959c27
labiss96 <cxz9080@gmail.com>
2020-02-25 01:04:36 +0900

    session 게시판 과제 추가기능 구현중..

--- Backend/KMU_likelion/board/serializers.py
+++ Backend/KMU_likelion/board/serializers.py
@@ -9,11 +9,12 @@ from .models import (CareerBoard, NoticeBoard, NoticeBoardComment, QnABoard,
 class AssignmentSerializer(serializers.ModelSerializer):
     author_name = serializers.ReadOnlyField(source='user_id.username')
     session_type = serializers.HiddenField(default='A')
+    # lectures = serializers.ReadOnlyField(source='lecture')
 
     class Meta:
         model = Session
         fields = ['id', 'author_name', 'title', 'user_id', 'body', 'score_types',
-                  'pub_date', 'update_date', 'session_type', 'lecture', 'deadline']
+                  'pub_date', 'update_date', 'session_type', 'lecture', 'deadline'] #'deadline'
 
 
 # Session type=LECTURE Serializer
