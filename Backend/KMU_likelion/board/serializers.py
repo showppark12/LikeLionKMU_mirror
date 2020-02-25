913c5f0f646e11a4afcd6715e4ef945fc624a71c
labiss96 <cxz9080@gmail.com>
2020-02-25 21:52:04 +0900

    assignment crud를 session detail page로 분리. 현재 과제추가만 구현됨.

--- Backend/KMU_likelion/board/serializers.py
+++ Backend/KMU_likelion/board/serializers.py
@@ -12,7 +12,7 @@ class AssignmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Session
         fields = ['id', 'author_name', 'title', 'user_id', 'body', 'score_types',
-                  'pub_date', 'update_date', 'session_type', 'lecture', 'deadline']  # 'deadline'
+                  'pub_date', 'update_date', 'session_type', 'lecture']  # 'deadline'
 
 
 # Session type=LECTURE Serializer
