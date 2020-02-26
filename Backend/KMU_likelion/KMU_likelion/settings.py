c1d8b035531512594cb3e799259d6dd54b8245c6
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2020-02-26 17:43:24 +0900

    페이지네이션 없앰

--- Backend/KMU_likelion/KMU_likelion/settings.py
+++ Backend/KMU_likelion/KMU_likelion/settings.py
@@ -156,7 +156,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 20,
     'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S",
 }
