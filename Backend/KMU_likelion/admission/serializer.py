d024602d6f5e2ca9a2fbab328381a1a950ac8d6a
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2020-02-27 02:09:25 +0900

    evaulation 이미지 삽입!

--- Backend/KMU_likelion/admission/serializer.py
+++ Backend/KMU_likelion/admission/serializer.py
@@ -16,7 +16,7 @@ class AnswerSerializer(serializers.ModelSerializer):
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
-    answer = AnswerSerializer(source= "application_answer", many = True)
+    answer = AnswerSerializer(source= "application_answer", many = True, read_only = True)
     class Meta:
         model = Application
         fields =  ['id', 'name', 'student_id','phone_number','birth', 'sex', 'major', 'email', 'pw','status','pub_date','update_date','answer']
@@ -25,7 +25,9 @@ class ApplicationSerializer(serializers.ModelSerializer):
 class EvaluationSerializer(serializers.ModelSerializer):
     user_name = serializers.ReadOnlyField(source='user_id.username')
     application_name = serializers.ReadOnlyField(source='application_id.name')
+    user_img = serializers.ImageField(
+        source='user_id.img', read_only=True, use_url=True)
 
     class Meta:
         model = Evaluation
-        fields = ['id', 'user_name', 'user_id','full_name','application_name', 'application_id', 'body', 'score', 'pub_date']
+        fields = ['id', 'user_name', 'user_id','full_name','application_name', 'application_id', 'body', 'score', 'pub_date','user_img']
