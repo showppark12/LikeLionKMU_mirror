ef38a153b6a13ebea5465b56f6b4889d7ea8bc1b
Jung-woo Lee <you@example.com>
2020-03-03 14:26:38 +0900

    ang

--- Backend/KMU_likelion/accounts/serializers.py
+++ Backend/KMU_likelion/accounts/serializers.py
@@ -21,7 +21,8 @@ class CreateUserSerializer(serializers.ModelSerializer):
             username=validated_data["username"],password= validated_data["password"],first_name=validated_data["first_name"],
             last_name=validated_data["last_name"], email=validated_data["email"], is_staff=validated_data["is_staff"], is_active=validated_data["is_active"],
             is_superuser=validated_data["is_superuser"],major=validated_data["major"],student_id=validated_data["student_id"],
-            user_type=validated_data["user_type"], start_number=validated_data["start_number"], sns_id=validated_data["sns_id"]
+            user_type=validated_data["user_type"], start_number=validated_data["start_number"], sns_id=validated_data["sns_id"],
+            img=validated_data["img"]
            )
         return user
 
