9a07548b038a229b713def494539ffa403dfc235
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2020-03-03 14:20:06 +0900

    이미지 왜 안돼냐

--- Backend/KMU_likelion/board/serializer_fields.py
+++ Backend/KMU_likelion/board/serializer_fields.py
@@ -18,6 +18,18 @@ class RecursiveSerializer(serializers.Serializer):
 
 
 class Base64ImageField(serializers.ImageField):
+    def decode_base64(data, altchars=b'+/'):
+        """Decode base64, padding being optional.
+
+        :param data: Base64 data as an ASCII byte string
+        :returns: The decoded byte string.
+
+        """
+        data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data) # normalize
+        missing_padding = len(data) % 4
+        if missing_padding:
+            data += b'='* (4 - missing_padding)
+        return base64.b64decode(data, altchars)
 
     def to_internal_value(self, data):
 
